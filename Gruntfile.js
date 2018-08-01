05408b770bf1ce47278f5933959ead4a85991b94
Uche Ozoemena <ucheozoemena@gmail.com>
2018-08-01 07:36:23 +0100

    Create images for md-sized viewport

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,9 @@ module.exports = function(grunt) {
       // I focused on mobile displays to enforce a mobile-first
       // priority and minimize use cases that I had to account for.
 
+      // the selected widths and heights are based my CSS declarations and
+      // browser testing to determine what is actually ideal
+
       // task target for logo for small breakpoint
       small_logo: {
         options: {
@@ -96,6 +99,66 @@ module.exports = function(grunt) {
           cwd: 'images_sm/',
           dest: 'images_resp/sm/'
         }]
+      },
+
+      // task target for my pic for medium breakpoint
+      med_my_pic: {
+        options: {
+          // engine: 'im', <-- commented out for Windows (Udacity's instructions)
+          separator: '_',
+          sizes: [
+            {
+              quality: 60,
+              width: 252,
+              density: 150,
+              name: '1x'
+            },
+            {
+              quality: 60,
+              width: 504,
+              density: 300,
+              name: '2x'
+            }
+          ]
+        },
+
+        files: [{
+          expand: true,
+          // process my pic
+          src: ['real_uche.jpg'],
+          cwd: 'images_md/',
+          dest: 'images_resp/md/'
+        }]
+      },
+
+      // task target for Featured Work images for medium breakpoint
+      med_featured_work: {
+        options: {
+          // engine: 'im', <-- commented out for Windows (Udacity's instructions)
+          separator: '_',
+          sizes: [
+            {
+              quality: 60,
+              width: 210,
+              density: 150,
+              name: '1x'
+            },
+            {
+              quality: 60,
+              width: 420,
+              density: 300,
+              name: '2x'
+            }
+          ]
+        },
+
+        files: [{
+          expand: true,
+          // process Featured Work images
+          src: ['*.{jpg,png}', '!bitmoji_uche.jpg', '!real_uche.jpg'],
+          cwd: 'images_md/',
+          dest: 'images_resp/md/'
+        }]
       }
     },
 
@@ -144,7 +207,9 @@ module.exports = function(grunt) {
   grunt.registerTask('med_imgs', [
     'clean:med',
     'mkdir:med',
-    'responsive_images:med_logo'
+    'responsive_images:med_logo',
+    'responsive_images:med_my_pic',
+    'responsive_images:med_featured_work'
   ]);
 
 };
